031fdcb29ae6b21ffafc13859c0b394cfd1ea436
Tien <tieeeeen1994@gmail.com>
2024-08-23 03:42:36 +0800

    chore: Refactor Patch_GameScript_UpdateMana transpiler method

--- Patches/Patch_GameScript_UpdateMana.cs
+++ Patches/Patch_GameScript_UpdateMana.cs
@@ -1,4 +1,5 @@
 ﻿using GadgetCore.API;
+using GadgetCore.Util;
 using HarmonyLib;
 using System.Collections.Generic;
 using System.Reflection;
@@ -14,32 +15,47 @@ namespace MoreCombatChips.Patches
     [HarmonyGadget("More Combat Chips")]
     public static class Patch_GameScript_UpdateMana
     {
-        [HarmonyTranspiler]
-        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        private static FieldInfo CurAugment
         {
-            var codes = new List<CodeInstruction>(instructions);
-            var modifiedCodes = new List<CodeInstruction>();
-            var matcherOperand = typeof(Menuu).GetField("curAugment", BindingFlags.Public | BindingFlags.Static);
-            var manaOperand = typeof(GameScript).GetField("mana", BindingFlags.Public | BindingFlags.Static);
-            var maxManaOperand = typeof(GameScript).GetField("maxmana", BindingFlags.Public | BindingFlags.Static);
-            for (int i = 0; i < codes.Count; i++)
+            get => typeof(Menuu).GetField("curAugment", BindingFlags.Public | BindingFlags.Static);
+        }
+
+        private static FieldInfo Mana
+        {
+            get => typeof(GameScript).GetField("mana", BindingFlags.Public | BindingFlags.Static);
+        }
+
+        private static FieldInfo MaxMana
+        {
+            get => typeof(GameScript).GetField("maxmana", BindingFlags.Public | BindingFlags.Static);
+        }
+
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> insn, ILGenerator il)
+        {
+            var p = TranspilerHelper.CreateProcessor(insn, il);
+            if (MoreCombatChips.GlibglobHatChange)
             {
-                if (MoreCombatChips.GlibglobHatChange &&
-                    codes[i].opcode == OpCodes.Ldsfld && codes[i].operand == matcherOperand &&
-                    codes[i + 1].opcode == OpCodes.Ldc_I4_7 &&
-                    codes[i + 2].opcode == OpCodes.Bne_Un &&
-                    codes[i + 3].opcode == OpCodes.Ldc_I4_0 &&
-                    codes[i + 4].opcode == OpCodes.Stsfld && codes[i + 4].operand == manaOperand &&
-                    codes[i + 5].opcode == OpCodes.Ldc_I4_0 &&
-                    codes[i + 6].opcode == OpCodes.Stsfld && codes[i + 6].operand == maxManaOperand)
+                var ilRef = p.FindRefByInsns(new CodeInstruction[]
                 {
-                    MoreCombatChips.Log("Patch_GameScript_UpdateMana: Remove GLibglob Hat 0 Mana effects!");
-                    codes[i + 7].labels.AddRange(codes[i].labels);
-                    i += 7;
+                new CodeInstruction(OpCodes.Ldsfld, CurAugment),
+                new CodeInstruction(OpCodes.Ldc_I4_7),
+                new CodeInstruction(OpCodes.Bne_Un),
+                new CodeInstruction(OpCodes.Ldc_I4_0),
+                new CodeInstruction(OpCodes.Stsfld, Mana),
+                new CodeInstruction(OpCodes.Ldc_I4_0),
+                new CodeInstruction(OpCodes.Stsfld, MaxMana)
+                });
+                if (ilRef == null)
+                {
+                    MoreCombatChips.Log("Patch_GameScript_UpdateMana: Transpiler could not find any reference point.");
                 }
-                modifiedCodes.Add(codes[i]);
+                else
+                {
+                    p.RemoveInsns(ilRef, 7);
+                }
             }
-            return modifiedCodes;
+            return p.Insns;
         }
     }
 }
